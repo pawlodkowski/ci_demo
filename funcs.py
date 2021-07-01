d271a20f17ba3f8b2fb3ee242eb02219900b2929
Paul Wlodkowski <pawlodkowski@gmail.com>
2021-07-01 09:25:57 +0200

    tests

---
+++ funcs.py
@@ -0,0 +1,13 @@
+import random
+
+def add_one(x):
+    return x + 1
+
+
+def recommend_movie(movie_list):
+
+    if type(movie_list) != list: 
+        raise TypeError('The input has to be a list, you fool!')
+    rec = random.choice(movie_list)
+    return rec.upper()
+
